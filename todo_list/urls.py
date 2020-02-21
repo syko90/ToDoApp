d6118ec641b1e261978ec265ed9578ca629fefe6
psycho <kurantowicz.maciej@gmail.com>
2020-02-21 13:01:02 +0100

    Koljeny push

--- todo_list/urls.py
+++ todo_list/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
 ]
