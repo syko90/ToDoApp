16ea3396063dff8adb88d6a4f95939d38cba9295
psycho <kurantowicz.maciej@gmail.com>
2020-02-21 18:23:18 +0100

    the end

--- todo_list/views.py
+++ todo_list/views.py
@@ -38,3 +38,16 @@ def uncross(request, list_id):
     item.save()
     return redirect('home')
 
+def edit(request, list_id):
+    if request.method == 'POST':
+        item = List.objects.get(pk=list_id)
+        form = ListForm(request.POST or None, instance=item)
+        if form.is_valid():
+            form.save()
+            messages.success(request, ('Element został edytowany '))
+        return redirect('home')
+    else:          
+        item = List.objects.get(pk=list_id)
+        return render(request, 'edit.html', {'item': item})
+
+
